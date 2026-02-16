b131369bf51421e08936255f92c04d48cee8bbe5
wutname1 <jeremy12@gmail.com>
2026-02-16 13:07:51 -0600

    Revert "Portraits now respond to clicks for targeting and menus"
    This reverts commit 59c9030ecbf3ec7455895e85817ca395e2952b1d.

--- Modules/UnitFrames/Elements/Portrait.lua
+++ Modules/UnitFrames/Elements/Portrait.lua
@@ -37,50 +37,17 @@ local function Build(frame, DB)
 	Portrait2D:Hide()
 	frame.Portrait2D = Portrait2D
 
-	-- Click overlay: transparent secure button on top of the portrait for right-click targeting/menu
-	local clickOverlay = CreateFrame('Button', nil, frame, 'SecureUnitButtonTemplate')
-	clickOverlay:SetAttribute('unit', frame.unitOnCreate)
-	clickOverlay:SetAttribute('*type1', 'target')
-	clickOverlay:SetAttribute('*type2', 'togglemenu')
-	clickOverlay:RegisterForClicks('AnyDown')
-	clickOverlay:SetFrameStrata('LOW')
-	clickOverlay:SetFrameLevel(10)
-	clickOverlay:EnableMouse(true)
-	clickOverlay:Hide()
-
-	-- Register with Clique for click-casting support
-	_G.ClickCastFrames = _G.ClickCastFrames or {}
-	_G.ClickCastFrames[clickOverlay] = true
-
-	-- Tooltip support: show unit tooltip on hover
-	clickOverlay:SetScript('OnEnter', function(self)
-		if UnitFrame_OnEnter then
-			UnitFrame_OnEnter(frame)
-		end
-	end)
-	clickOverlay:SetScript('OnLeave', function(self)
-		if UnitFrame_OnLeave then
-			UnitFrame_OnLeave(frame)
-		end
-	end)
-
-	frame.PortraitClickOverlay = clickOverlay
 	frame.Portrait = Portrait3D
 end
 
 ---@param frame table
 local function Update(frame)
 	local DB = frame.Portrait.DB
-	local clickOverlay = frame.PortraitClickOverlay
 
 	frame.Portrait3D:Hide()
 	frame.Portrait2D:Hide()
 	frame.Portrait3D:ClearAllPoints()
 	frame.Portrait2D:ClearAllPoints()
-	if clickOverlay then
-		clickOverlay:Hide()
-		clickOverlay:ClearAllPoints()
-	end
 	if not DB.enabled then
 		return
 	end
@@ -115,17 +82,6 @@ local function Update(frame)
 	else
 		frame.Portrait = frame.Portrait2D
 		frame.Portrait2D:Show()
-	end
-
-	-- Position click overlay on top of the active portrait (skip overlay mode - main frame already handles clicks)
-	if clickOverlay and DB.position ~= 'overlay' then
-		-- Can't use SetAllPoints on protected frame to a PlayerModel/Texture region
-		-- Instead, use two anchor points to match the portrait's bounds
-		local portrait = frame.Portrait
-		clickOverlay:SetPoint('TOPLEFT', portrait, 'TOPLEFT')
-		clickOverlay:SetPoint('BOTTOMRIGHT', portrait, 'BOTTOMRIGHT')
-		clickOverlay:SetScale(DB.scale)
-		clickOverlay:Show()
 	end
 end
 
