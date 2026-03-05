8462c409d4ca2f0d8eff430539ae4adfca36cab6
wutname1 <jeremy12@gmail.com>
2026-03-04 22:26:13 -0600

    fixes: Quest indicators now show on target frame and nameplates
    The old tooltip-scanning plugin used APIs that don't exist on Retail.
    Switched to oUF's built-in QuestIndicator using UnitIsQuestBoss.

--- Modules/UnitFrames/Elements/QuestMob.lua
+++ Modules/UnitFrames/Elements/QuestMob.lua
@@ -3,39 +3,22 @@ local UF = SUI.UF
 ---@param frame table
 ---@param DB table
 local function Build(frame, DB)
-	local element = CreateFrame('Frame', frame:GetName() .. 'QuestIcons', frame)
-	element:SetSize(20, 20)
-	element:Hide()
+	local indicator = frame.raised:CreateTexture(nil, 'OVERLAY')
 
-	for _, object in ipairs({ 'Default', 'Item', 'Skull', 'Chat' }) do
-		local icon = element:CreateTexture(nil, 'BORDER', nil, 1)
-		icon.Text = element:CreateFontString(nil, 'OVERLAY')
-		SUI.Font:Format(icon.Text, 12, 'Nameplates')
-		icon:Hide()
-		icon:SetAllPoints(element)
-
-		element[object] = icon
-	end
-
-	-- QuestIcons.Item:SetTexCoord(unpack(E.TexCoords))
-	element.Chat:SetTexture([[Interface\WorldMap\ChatBubble_64.PNG]])
-	element.Chat:SetTexCoord(0, 0.5, 0.5, 1)
-
-	frame.QuestMob = element
+	-- oUF built-in QuestIndicator element uses UnitIsQuestBoss for updates
+	frame.QuestIndicator = indicator
+	-- SUI element system references by registered name
+	frame.QuestMob = indicator
 end
 
 ---@param unitName string
 ---@param OptionSet AceConfig.OptionsTable
 local function Options(unitName, OptionSet)
 	local function OptUpdate(option, val)
-		--Update memory
 		UF.CurrentSettings[unitName].elements.QuestMob[option] = val
-		--Update the DB
 		UF.DB.UserSettings[UF:GetPresetForFrame(unitName)][unitName].elements.QuestMob[option] = val
-		--Update the screen
 		UF.frames[unitName]:ElementUpdate('QuestMob')
 	end
-	--local DB = UF.CurrentSettings[unitName].elements.QuestMob
 end
 
 ---@type SUI.UF.Elements.Settings
